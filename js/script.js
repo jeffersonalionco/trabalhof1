e781d0c766dc1e83ae9160fc37fd201165e7ff28
Jefferson L. Alionco <jeffersonalionco@gmail.com>
2024-04-03 10:07:17 -0300

    Novas alterações

--- js/script.js
+++ js/script.js
@@ -5,11 +5,43 @@ function expandir(container) {
     } else {
         // Se o container não estiver expandido, expanda-o e recolha os outros containers
         container.classList.add("expandido");
-        var outrosContainers = document.querySelectorAll(".container1, .container2");
+        var outrosContainers = document.querySelectorAll(".container1, .container2, .container3");
         outrosContainers.forEach(function(element) {
             if (element !== container && element.classList.contains("expandido")) {
                 element.classList.remove("expandido");
             }
         });
     }
+}
+
+function moverCarro(direita) {
+    var carro = document.querySelector('.carro');
+    var faixa = document.querySelector('.faixa');
+    if (direita) {
+        carro.style.left = "80%"
+
+    } else {
+        carro.style.left = '30%';
+    }
+}
+
+
+function gerarExplosao() {
+    var carro = document.querySelector('.carro');
+    carro.src = './img/explosao1.gif'; // Substituir pelo caminho da imagem de explosão
+    carro.classList.add('explosao');
+    carro.style.left = carro.offsetLeft + 'px';
+    carro.style.top = carro.offsetTop + 'px';
+    var faixa = document.querySelector('.faixa')
+    faixa.style.backgroundImage = "url('./img/bg_footer.avif')";
+
+    var formula1 = document.querySelector('.formula1')
+    formula1.style.opacity = 100;
+    
+    
+    carro.style.opacity = 0;
+    
+    // Remover o evento de mover o carro
+    carro.parentNode.onmouseover = null;
+    carro.parentNode.onmouseout = null;
 }
